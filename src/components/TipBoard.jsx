fccf50a2ab19fc61e613f1a7f0f3a9602652eba9
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-06-25 19:23:57 +0100

    🎨 ui(app): rendering tip info and resetting

--- src/components/TipBoard.jsx
+++ src/components/TipBoard.jsx
@@ -1,4 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { resetForm } from '../store';
+
 const TipBoard = () => {
+  const dispatch = useDispatch();
+  const { bill, numOfPeople, tip } = useSelector((store) => store.form);
+
+  const numBill = Number(bill);
+  const numPeople = Number(numOfPeople);
+  const numTip = Number(tip);
+
+  const isValid = !!(numBill > 0 && numPeople > 0 && numTip >= 0);
+
+  const tipAmount = isValid
+    ? ((numBill * (numTip / 100)) / numPeople).toFixed(2)
+    : null;
+
+  const total = isValid
+    ? (numBill / numPeople + (numBill * (numTip / 100)) / numPeople).toFixed(2)
+    : null;
+
   return (
     <section className="grid grid-rows-[auto_1fr] gap-10 bg-cyan-500 pt-10 px-6 pb-6 rounded-xl">
       <div className="grid grid-rows-2 gap-7">
@@ -12,7 +32,7 @@ const TipBoard = () => {
               person
             </span>
           </h2>
-          <p className="text-3xl text-cyan-600">$0.00</p>
+          <p className="text-3xl text-cyan-600">${`${tipAmount || '0.00'}`}</p>
         </div>
         <div className="flex flex-row items-start justify-between">
           <h2 className="text-base leading-tight text-white">
@@ -24,10 +44,13 @@ const TipBoard = () => {
               person
             </span>
           </h2>
-          <p className="text-3xl text-cyan-600">$0.00</p>
+          <p className="text-3xl text-cyan-600">${`${total || '0.00'}`}</p>
         </div>
       </div>
-      <button className="w-full text-center text-xl rounded bg-cyan-600 text-cyan-500 py-2 cursor-pointer hover:bg-cyan-200 transition ease-in">
+      <button
+        disabled={!isValid}
+        className="disabled:opacity-15 w-full text-center text-xl rounded bg-cyan-600 text-cyan-500 py-2 cursor-pointer hover:bg-cyan-200 transition ease-in"
+        onClick={() => dispatch(resetForm())}>
         RESET
       </button>
     </section>
